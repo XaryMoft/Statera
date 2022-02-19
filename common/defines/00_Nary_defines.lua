171d93c4354d52f45406dba09c54903d61030189
Xary Moft <68449133+XaryMoft@users.noreply.github.com>
2022-02-19 13:45:10 +0000

    Added the first lines of code
    Reduced slacken manpower from professionalism from 20% to 10%.

---
+++ common/defines/00_Nary_defines.lua
@@ -0,0 +1,6 @@
+-- Format for overwriting define values, this is a custom format for the mod:
+
+
+
+
+NDefines.NMilitary.SLACKEN_MANPOWER_INCREASE = 1 -- from 2, to shorten wars by reducing the reservoir of manpower of professionalism.
