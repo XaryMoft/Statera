2e50be6eeb43b5900371d1e01b9a7d01255bbffa
Xary Moft <68449133+XaryMoft@users.noreply.github.com>
2022-02-24 15:32:34 +0000

    Updated War Taxes
    War taxes are now free

--- common/defines/00_Nary_defines.lua
+++ common/defines/00_Nary_defines.lua
@@ -4,3 +4,6 @@
 
 
 NDefines.NMilitary.SLACKEN_MANPOWER_INCREASE = 0.5 -- from 2, to shorten wars by reducing the reservoir of manpower of professionalism.
+
+NDefines.NCountry.PS_RAISE_WAR_TAXES	= 0 -- From 50, this means war taxes are now don't cost mil points.
+
