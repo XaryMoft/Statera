f7f701037e0a45ac28ed1bdb61400c45b168fd40
Xary Moft <68449133+XaryMoft@users.noreply.github.com>
2022-02-24 13:28:49 +0000

    Testing war taxes

--- common/defines/00_Nary_defines.lua
+++ common/defines/00_Nary_defines.lua
@@ -4,3 +4,5 @@
 
 
 NDefines.NMilitary.SLACKEN_MANPOWER_INCREASE = 0.5 -- from 2, to shorten wars by reducing the reservoir of manpower of professionalism.
+
+NDefines.NCountry.PS_RAISE_WAR_TAXES	= 0 -- from 50
