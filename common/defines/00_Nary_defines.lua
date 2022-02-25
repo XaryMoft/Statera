8637af61c9a4b87d4d446292a37348169fff5412
Xary Moft <68449133+XaryMoft@users.noreply.github.com>
2022-02-25 16:49:22 +0000

    War taxes update

--- common/defines/00_Nary_defines.lua
+++ common/defines/00_Nary_defines.lua
@@ -5,5 +5,5 @@
 
 NDefines.NMilitary.SLACKEN_MANPOWER_INCREASE = 0.5 -- from 2, to shorten wars by reducing the reservoir of manpower of professionalism.
 
-NDefines.NCountry.PS_RAISE_WAR_TAXES	= 0 -- From 2 (wiki is outaded), this means war taxes now don't cost mil points, might revert this to cost 1 mil point instead
+NDefines.NCountry.PS_RAISE_WAR_TAXES	= 1 -- From 2 (wiki "50" is outaded), this means war taxes now cost 1 mil point pur month
 
