1daba77e8c1acc2f33faab04b3c2b593e1a478f4
Xary Moft <68449133+XaryMoft@users.noreply.github.com>
2022-02-27 16:51:53 +0000

    Army maintenance and more base papal influence

--- common/defines/00_Nary_defines.lua
+++ common/defines/00_Nary_defines.lua
@@ -7,3 +7,7 @@ NDefines.NMilitary.SLACKEN_MANPOWER_INCREASE = 0.5 -- from 2, to shorten wars by
 
 NDefines.NCountry.PS_RAISE_WAR_TAXES	= 1 -- From 2 (wiki "50" is outaded), this means war taxes now cost 1 mil point pur month
 
+NDefines.NEconomy.LAND_TECH_MAINTENANCE_IMPACT = 0.04 -- from 0.02, to help prevent infinite money mid and late game
+NDefines.NEconomy.LAND_TECH_MERC_MAINTENANCE_IMPACT = 0.12 -- from 0.08, to help prevent infinite money mid and late game
+
+NDefines.NReligion.YEARLY_PAPAL_INFLUENCE_CATHOLIC = 1 -- from 0.5, so Catholic religion is more viable for nations unable to acquire cardinals
