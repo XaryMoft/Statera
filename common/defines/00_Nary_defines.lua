0a28c9f3d7463f132d1d9f71ae3392b08df8cc84
Xary Moft <68449133+XaryMoft@users.noreply.github.com>
2022-03-10 00:37:07 +0000

    Skill gain for generals improved from irrelavancy

--- common/defines/00_Nary_defines.lua
+++ common/defines/00_Nary_defines.lua
@@ -4,8 +4,10 @@
 
 
 NDefines.NMilitary.SLACKEN_MANPOWER_INCREASE = 0.5 -- from 2, to shorten wars by reducing the reservoir of manpower of professionalism.
+NDefines.NMilitary.ARMY_DRILL_SKILL_MONTHS = 60 -- from 120, to increase the probability generals improve skill from drilling
 
-NDefines.NCountry.PS_RAISE_WAR_TAXES	= 1 -- From 2 (wiki "50" is outaded), this means war taxes now cost 1 mil point pur month
+
+NDefines.NCountry.PS_RAISE_WAR_TAXES = 1 -- From 2 (wiki "50" is outaded), this means war taxes now cost 1 mil point pur month
 NDefines.NCountry.PS_SCORCH_EARTH = 10 -- from 5, since 5 it's too litle and dissipates very quickly
 
 
