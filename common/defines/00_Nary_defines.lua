b216a87d46db6dc21c141fe7ec74ac9de80cc7cc
Xary Moft <68449133+XaryMoft@users.noreply.github.com>
2022-03-14 12:41:39 +0000

    fixed defines

--- common/defines/00_Nary_defines.lua
+++ common/defines/00_Nary_defines.lua
@@ -5,7 +5,7 @@
 
 NDefines.NMilitary.SLACKEN_MANPOWER_INCREASE = 0.5 -- from 2, to shorten wars by reducing the reservoir of manpower of professionalism.
 NDefines.NMilitary.ARMY_DRILL_SKILL_MONTHS = 60 -- from 120, to increase the probability generals improve skill from drilling
-NDefines.NMilitary.SUPPLY_DEPOT_MIL_COST = 10 ~~ from 20, to encourage people to build supply depots due to the attrition/supply changes
+NDefines.NMilitary.SUPPLY_DEPOT_MIL_COST = 10 -- from 20, to encourage people to build supply depots due to the attrition/supply changes
 
 NDefines.NCountry.PS_RAISE_WAR_TAXES = 1 -- From 2 (wiki "50" is outaded), this means war taxes now cost 1 mil point pur month
 NDefines.NCountry.PS_SCORCH_EARTH = 10 -- from 5, since 5 it's too litle and dissipates very quickly
