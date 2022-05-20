afa93955850608dcfb29b37d7fac9b2f0332da81
Xary Moft <68449133+XaryMoft@users.noreply.github.com>
2022-05-20 02:22:32 +0100

    Tons of changes, requires testing

--- common/defines/00_Nary_defines.lua
+++ common/defines/00_Nary_defines.lua
@@ -4,12 +4,15 @@ NDefines = {
 NDiplomacy = {
     CHANGE_RIVAL_YEARS = 10 -- from 25 years, allowing more flexibility in diplomacy
     SUPPORT_REBELS_EFFECT = 25 -- from 10, so rebel support eff isn't useless
+    SPY_NETWORK_TECH_EFFECT = -0.1 -- from -5% tech cost reduction at 100 spy network, increased because it's too weak if you have lower values of spy network and 100 spy network is a lot
+    INFILTRATE_ADMINISTRATION_COST = 80 -- from 40, it's too cheap to do it and spam it, since it's unlocked in earlier tech it needs to be adjusted
 }
 
 NMilitary = {
     NMilitary.SLACKEN_MANPOWER_INCREASE = 1 -- from 2 years worth of manpower, to shorten wars by reducing the reservoir of manpower of professionalism.
     NMilitary.ARMY_DRILL_SKILL_MONTHS = 60 -- from 120, to increase the probability generals improve skill from drilling
     NMilitary.SUPPLY_DEPOT_MIL_COST = 10 -- from 20, to encourage people to build supply depots due to the attrition/supply changes
+    MERCENARY_COMPANY_MANPOWER_PER_REGIMENT = 1.5,	-- Manpower reserve factor (multiplied by 1000 per regiment), from 2.0
 }
 
 NCountry = {
@@ -30,12 +33,12 @@ NReligion = {
 }
 
 NAI = {
-    DEBASE_THRESHOLD = 0 -- AI wont debase if it has more gold than this (in theory at 0 the AI wont debase it's currency if it's making money or just lost a huge ammount).
+    DEBASE_THRESHOLD = 0 -- AI wont debase if it has more gold than this (in theory at 0 the AI wont debase it's currency if it's making money, it only will do it if it just lost a huge ammount).
 }
 
 NGovernment = {
-    GOVERNMENT_REFORM_COST_CENTRALIZE_STATE = 50.0 -- Government Reform cost for centralize state
-    CENTRALIZE_STATE_ADM_COST = 50 -- ADM cost for centralize state
+    GOVERNMENT_REFORM_COST_CENTRALIZE_STATE = 20.0 -- Government Reform cost for centralize state
+    CENTRALIZE_STATE_ADM_COST = 75 -- ADM cost for centralize state
 }
 
 }
