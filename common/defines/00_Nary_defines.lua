654b19bf05cc2b559553de8d224f9fe977216229
Xary Moft <68449133+XaryMoft@users.noreply.github.com>
2022-08-29 23:29:15 +0100

    First pack of changes

--- common/defines/00_Nary_defines.lua
+++ common/defines/00_Nary_defines.lua
@@ -5,6 +5,8 @@ NDefines.NDiplomacy.CHANGE_RIVAL_YEARS = 10 -- from 25 years, allowing more flex
 NDefines.NDiplomacy.SUPPORT_REBELS_EFFECT = 25 -- from 10, so rebel support eff isn't useless
 NDefines.NDiplomacy.SPY_NETWORK_TECH_EFFECT = -0.1 -- from -5% tech cost reduction at 100 spy network, increased because it's too weak if you have lower values of spy network and 100 spy network is a lot
 NDefines.NDiplomacy.INFILTRATE_ADMINISTRATION_COST = 90 -- from 40, it's too cheap to do it and spam it, it's also unlocked in earlier tech so it needs to be adjusted
+NDefines.NDiplomacy.SUPPORT_REBELS_COST = 30 -- from 60, this is temporary to test in 1.34
+NDefines.NDiplomacy.SPY_NETWORK_AE_EFFECT = -0.3 -- from -0.1, 1.34 change
 
 
 NDefines.NMilitary.SLACKEN_MANPOWER_INCREASE = 1 -- from 2 years worth of manpower, to shorten wars by reducing the reservoir of manpower of professionalism.
@@ -12,7 +14,6 @@ NDefines.NMilitary.ARMY_DRILL_SKILL_MONTHS = 60 -- from 120, to increase the pro
 NDefines.NMilitary.MARINE_SHOCK_DAMAGE_TAKEN = 0 -- from 0.10, using sailors is already a limiting factor to marines
 
 
-NDefines.NCountry.PS_RAISE_WAR_TAXES = 1 -- From 2, wiki "50" is outaded, this means war taxes now cost 1 mil point pur month
 NDefines.NCountry.PS_SCORCH_EARTH = 20 -- from 5, since 5 it's too litle and dissipates very quickly
 NDefines.NCountry.CORRUPTION_COST = 0.03 -- from 0.05
 NDefines.NCountry.MAXIMUM_CONDOTTIERI = 30 -- from 20, so it scalles better
